d74c9adb4f26ed21a96e9387e0afc917c281e727
rooslucas <rooslucas@live.nl>
2023-12-03 12:58:01 +0100

    fmt

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,9 +1,9 @@
 // use std::env;
 //use std::fs;
+use regex::Regex;
+use regex::RegexSet;
 use std::fs::read_to_string;
 use std::string;
-use regex::RegexSet;
-use regex::Regex;
 
 fn main() {
     let file_path = "./input/part_01.txt";
@@ -34,14 +34,18 @@ fn main() {
     // Part 2
 
     let mut all_numbers = Vec::new();
-    
+
     for line in &liness {
         all_numbers.push(part_2(&line));
     }
 
     let sum2: u32 = all_numbers.iter().sum();
-    println!("the total sum is: {}, {}, {}", sum2, all_numbers.len(), liness.len());
-
+    println!(
+        "the total sum is: {}, {}, {}",
+        sum2,
+        all_numbers.len(),
+        liness.len()
+    );
 }
 
 fn disect(instruction: &str) -> u32 {
@@ -52,16 +56,21 @@ fn disect(instruction: &str) -> u32 {
 
     let number_1 = splits[0].to_string() + splits[splits.len() - 1];
     //println!("{}", number_1);
-    let number_2 = [number_1.chars().next(), number_1.chars().last()].iter().flatten().collect::<String>();
+    let number_2 = [number_1.chars().next(), number_1.chars().last()]
+        .iter()
+        .flatten()
+        .collect::<String>();
     println!("{}", number_2);
     let number_final: u32 = number_2.trim().parse().expect("Split errorr");
 
     number_final
-
 }
 
 fn part_2(input: &str) -> u32 {
-    let re_numbers = RegexSet::new(&[r"[1-9]", r"one", r"two", r"three", r"four", r"five", r"six", r"seven", r"eight", r"nine"]).unwrap();
+    let re_numbers = RegexSet::new(&[
+        r"[1-9]", r"one", r"two", r"three", r"four", r"five", r"six", r"seven", r"eight", r"nine",
+    ])
+    .unwrap();
     let matches: Vec<_> = re_numbers.matches(input).iter().collect();
 
     let re_numbers = Regex::new(r"[1-9]").unwrap();
@@ -86,44 +95,49 @@ fn part_2(input: &str) -> u32 {
     let match_eight = re_eight.find(input);
     let match_nine = re_nine.find(input);
 
-    let all_matches = vec![match_num, match_one, match_two, match_three, match_four, match_five, match_six, match_seven, match_eight, match_nine];  
+    let all_matches = vec![
+        match_num,
+        match_one,
+        match_two,
+        match_three,
+        match_four,
+        match_five,
+        match_six,
+        match_seven,
+        match_eight,
+        match_nine,
+    ];
 
-    let mut high: usize = 0;
+    let mut high: usize = usize::MIN;
     let mut high_value: String = "hello".to_string();
-    let mut low: usize = 100;
+    let mut low: usize = usize::MAX;
     let mut low_value: String = "hello".to_string();
 
     for matchy in matches {
-
         if matchy == 0 {
             let numbers: Vec<_> = re_numbers.find_iter(input).collect();
             for num in numbers {
-                
                 if num.start() >= high {
                     high = num.start();
                     high_value = num.as_str().to_string();
-                    }
-               
+                }
+
                 if num.start() <= low {
                     low = num.start();
                     low_value = num.as_str().to_string();
-                    }
                 }
             }
-
-        else {
-
+        } else {
             if all_matches[matchy].unwrap().start() >= high {
                 high = all_matches[matchy].unwrap().start();
                 high_value = matchy.to_string();
-                }
-            
+            }
+
             if all_matches[matchy].unwrap().start() <= low {
                 low = all_matches[matchy].unwrap().start();
                 low_value = matchy.to_string();
-                }
             }
-
+        }
     }
 
     let number_1 = low_value.to_string() + &high_value;
@@ -132,5 +146,4 @@ fn part_2(input: &str) -> u32 {
     //let number_2 = [number_1.chars().next(), number_1.chars().last()].iter().flatten().collect::<String>();
     // println!("{}", number_2);
     number_1.trim().parse().expect("Split errorr")
-
 }
