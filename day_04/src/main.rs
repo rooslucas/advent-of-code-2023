6fa76c0df1dd2a265bf6b15e78e9a5be51d4a500
rooslucas <rooslucas@live.nl>
2023-12-04 13:35:06 +0100

    Add solution day 4 part 2

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -10,14 +10,23 @@ fn main() {
     // Part 1
     let mut total_points: u32 = 0;
 
-    for card in liness {
-        total_points += card_check(card);
+    for card in &liness {
+        total_points += card_check1(card);
     }
 
     println!("total points is {total_points}");
+
+    // Part 2
+    let mut total_scratchcards: u32 = liness.len().try_into().unwrap();
+
+    for card in 0..liness.len() {
+        total_scratchcards += card_check2(card, liness.clone());
+    }
+
+    println!("total points is {total_scratchcards}");
 }
 
-fn card_check(input: &str) -> u32 {
+fn card_check1(input: &str) -> u32 {
     let mut points: u32 = 0;
 
     let split_card: Vec<_> = input.split(":").collect();
@@ -39,4 +48,28 @@ fn card_check(input: &str) -> u32 {
     }
 
     points
+}
+
+fn card_check2(index: usize, file: Vec<&str>) -> u32 {
+    let mut extra_cards: u32 = 0;
+    let input = file[index];
+
+    let split_card: Vec<_> = input.split(":").collect();
+    let split_numbers: Vec<_> = split_card[1].split("|").collect();
+
+    let mut check_numbers: Vec<_> = split_numbers[0].split(" ").collect();
+    check_numbers.retain(|value| *value != "");
+    let mut own_numbers: Vec<_> = split_numbers[1].split(" ").collect();
+    own_numbers.retain(|value| *value != "");
+    let mut i = 1;
+
+    for numb in check_numbers {
+        if own_numbers.contains(&numb) {
+            extra_cards += 1;
+            extra_cards += card_check2(index + i, file.clone());
+            i += 1;
+        }
+    }
+
+    extra_cards
 }
