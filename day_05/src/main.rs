d169a4b194bc55d658a8e6320b142bec7fbec4d5
rooslucas <rooslucas@live.nl>
2023-12-06 17:10:58 +0100

    Add solution day 5 part 2

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::read_to_string;
-use std::slice::Split;
+use std::u64;
 
 #[derive(Debug)]
 struct Range {
@@ -7,35 +7,64 @@ struct Range {
     start_source: u64,
     length: u64,
 }
+impl Range {
+    fn range_check(&self, i: u64) -> u64 {
+        let end_source = self.start_source + self.length;
+
+        if (self.start_source..end_source).contains(&i) {
+            let diff = i - self.start_source;
+            return self.start_destination + diff;
+        } else {
+            return 0;
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Map {
     name: String,
     ranges: Vec<Range>,
 }
 
+impl Map {
+    fn check_ranges(&self, value: u64) -> u64 {
+        let mut check_list = Vec::new();
+        for range in &self.ranges {
+            check_list.push(range.range_check(value));
+        }
+
+        let sum: u64 = check_list.iter().sum();
+        if sum > 0 {
+            return sum;
+        } else {
+            return value;
+        }
+    }
+}
+
 fn main() {
-    let file_path = "./input/test.txt";
+    let file_path = "./input/day_05.txt";
     println!("In file {}", file_path);
 
     let binding = read_to_string(file_path).unwrap();
     let liness: Vec<_> = binding.lines().collect();
 
     let parsed = parse(liness);
-    println!("{:#?}", parsed);
+    // println!("{:#?}", parsed);
+
+    part_1(parsed.0, parsed.1)
 }
 
 fn parse(input: Vec<&str>) -> (Vec<&str>, Vec<Map>) {
-    let mut parsed_input = Vec::new();
+    let mut maps = Vec::new();
     let mut seeds: Vec<_> = input[0].split(" ").collect();
     seeds.remove(0);
-    println!("{:?}", seeds);
 
     let mut all: Vec<_> = input
         .split(|&e| e == "")
         .filter(|v| !v.is_empty())
         .collect();
     all.remove(0);
-    println!("{:?}", all);
 
     for map in all {
         let mut ranges = Vec::new();
@@ -49,14 +78,35 @@ fn parse(input: Vec<&str>) -> (Vec<&str>, Vec<Map>) {
             });
         }
 
-        let mut new_map = Map {
+        let new_map = Map {
             name: map[0].to_string(),
             ranges: ranges,
         };
 
-        println!("{:#?}", new_map);
-        parsed_input.push(new_map);
+        // println!("{:#?}", new_map);
+        maps.push(new_map);
     }
 
-    (seeds, parsed_input)
+    (seeds, maps)
+}
+
+fn part_1(seeds: Vec<&str>, maps: Vec<Map>) {
+    let mut locations = Vec::new();
+
+    for seed in seeds {
+        let mut current: u64;
+        let seed2 = seed.parse::<u64>().expect("Split errorr");
+        current = maps[0].check_ranges(seed2);
+        current = maps[1].check_ranges(current);
+        current = maps[2].check_ranges(current);
+        current = maps[3].check_ranges(current);
+        current = maps[4].check_ranges(current);
+        current = maps[5].check_ranges(current);
+        current = maps[6].check_ranges(current);
+
+        println!("{current}");
+        locations.push(current);
+    }
+    locations.sort();
+    println!("{:?}", locations);
 }
