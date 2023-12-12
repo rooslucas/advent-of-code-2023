60a9b48e24f49b97fbbaed2ec5af595fa73e90b2
rooslucas <rooslucas@live.nl>
2023-12-12 15:20:25 +0100

    Add solution part 1 day 11

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -1,18 +1,28 @@
 use array2d::{Array2D, Error};
+use itertools::Itertools;
 use std::{char, fs::read_to_string};
 
+struct Galaxy {
+    number: i32,
+    x: i32,
+    y: i32,
+}
+
 fn main() {
-    let file_path = "./input/test.txt";
+    let file_path = "./input/day_11.txt";
     println!("In file {}", file_path);
 
     let binding = read_to_string(file_path).unwrap();
     let liness: Vec<_> = binding.lines().collect();
     let matrix: Vec<Vec<char>> = liness.iter().map(|x| x.chars().collect()).collect();
+    let parsed = parse(matrix);
+    println!("{}", parsed.0.len());
 
-    parse(matrix).iter().for_each(|x| println!("{:?}", x));
+    // parse(matrix).iter().for_each(|x: &Vec<char>| println!("{:?}", x));
+    println!("{:?}", (part_1(parsed.0, parsed.1)));
 }
 
-fn parse(mut input: Vec<Vec<char>>) -> Vec<Vec<char>> {
+fn parse(mut input: Vec<Vec<char>>) -> (Vec<Galaxy>, i32) {
     // double empty lines
     // rows
 
@@ -22,6 +32,7 @@ fn parse(mut input: Vec<Vec<char>>) -> Vec<Vec<char>> {
             index.push(x);
         }
     }
+    println!("{}", index.len());
 
     for i in 0..index.len() {
         let ind: usize = index[i] + i;
@@ -43,7 +54,8 @@ fn parse(mut input: Vec<Vec<char>>) -> Vec<Vec<char>> {
     let s = Array2D::from_columns(&columns).unwrap();
     let mut matrix = Vec::new();
     for row in s.as_rows() {
-        matrix.push(row);
+        let n_row: Vec<_> = row.iter().map(|x| x.to_string()).collect();
+        matrix.push(n_row);
     }
 
     // matrix.iter().for_each(|x| println!("{:?}", x));
@@ -51,21 +63,59 @@ fn parse(mut input: Vec<Vec<char>>) -> Vec<Vec<char>> {
 
     // change # to numbers
     let mut num = 1;
+    let mut galaxies = Vec::new();
 
     for x in 0..matrix.len() {
         for y in 0..matrix[x].len() {
-            if matrix[x][y] == '#' {
-                let vec: Vec<char> = num.to_string().chars().collect();
+            if matrix[x][y] == "#" {
+                matrix[x][y] = num.to_string();
+                galaxies.push(Galaxy {
+                    number: num,
+                    x: x.try_into().unwrap(),
+                    y: y.try_into().unwrap(),
+                });
                 num += 1;
-                matrix[x][y] = vec[0];
             }
         }
     }
 
-    // matrix.iter().for_each(|x| println!("{:?}", x));
+    matrix.iter().for_each(|x| println!("{:?}", x));
 
     // return matrix
-    matrix
+    (galaxies, num)
 }
 
-fn find_path() {}
+fn part_1(galaxies: Vec<Galaxy>, num: i32) -> i32 {
+    // find all unique combinations of galaxies
+    let all_numbs: Vec<_> = (1..num).collect();
+    // println!("{:?}", all_numbs);
+
+    let all_combinations: Vec<_> = all_numbs
+        .iter()
+        .tuple_combinations()
+        .map(|(a, b)| (a, b))
+        .unique()
+        .collect();
+
+    // println!("{:?}", all_combinations);
+    let mut total = 0;
+
+    for combi in all_combinations {
+        let g1: Vec<&Galaxy> = galaxies.iter().filter(|g| g.number == *combi.0).collect();
+        let g2: Vec<&Galaxy> = galaxies.iter().filter(|g| g.number == *combi.1).collect();
+        total += find_path(g1[0], g2[0]);
+    }
+
+    // find index of those galaxies (x, y) (x, y)
+    // find path
+    // return sum of numbers.
+    total.try_into().unwrap()
+}
+
+fn find_path(xy_1: &Galaxy, xy_2: &Galaxy) -> i32 {
+    let x = (xy_2.x - xy_1.x).abs();
+    let y = (xy_2.y - xy_1.y).abs();
+    // println!("{}", x + y);
+
+    x + y
+}
